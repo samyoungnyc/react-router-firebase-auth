683f20d021dfe23f38c26b9430a62c8ea00fc026
Froren <r@sager>
2017-07-04 13:36:09 -0400

    Restructured async code

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import { getData } from './Data.js'
 
 export default class Dashboard extends Component {
-  render () {
 
-    getData();
+  state = {
+  	data: null,
+  	loading: true
+  }
+
+  componentDidMount() {
+  	getData().then(result => {
+  		this.setState({
+  			data: result,
+  			loading: false
+  		})
+  	});
+  }
+
+  render () {
 
     return (
       <div>
-        Dashboard. This is a protected page. You can only see this if you're authed.
+        {this.state.loading ? "Dashboard. This is a protected page. You can only see this if you're authed.": JSON.stringify(this.state.data)} 
       </div>
     )
   }
--- src/components/protected/Data.js
+++ src/components/protected/Data.js
@@ -1,23 +1,27 @@
 import { ref } from '../../config/constants'
 
 export function getData () {
-	var talentItems = [];
+
 	var talentRef = ref.child('future');
-		talentRef.once("value", function(snapshot) {
-			snapshot.forEach((child)=> {
-				var data = {	
-					'artist': child.val().artist,
-					'author': child.val().author,
-					'projectTitle': child.val().projectTitle,
-					'text': child.val().text
+	return talentRef.once("value", function(snapshot) {
+		return snapshot;
+	}).then(function(snapshot) {
+
+		let talentItems = [];
+
+		snapshot.forEach((child)=> {
+			var data = {	
+				'artist': child.val().artist,
+				'author': child.val().author,
+				'projectTitle': child.val().projectTitle,
+				'text': child.val().text
 			};
-				talentItems.push({data})
-			})
-			console.log(talentItems[0].data.artist);
-			// return talentItems;
-		}, function (error) {
-			console.log("Error: " + error.code);
+
+			talentItems.push(data);
 		});
-		// console.log(talentItems);
+			
 		return talentItems;
+	}).catch(function(error) {
+		console.log("Error: " + error.code);
+	});
 }
